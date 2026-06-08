07b211d4073f418b6da3b3f24bd2bc4c9890d718
Jamie <clashrush006@gmail.com>
2026-06-08 23:44:01 +0100

    Update BoostQueue.lua

--- BoostQueue.lua
+++ BoostQueue.lua
@@ -1,4 +1,4 @@
 if AlreadyQueued then return end; AlreadyQueued = true; print("Waiting for game..");
 if not game:IsLoaded() then game.Loaded:Wait() end; 
 task.wait(5);
-loadstring(game:HttpGet("https://url"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/LuvNarcc/ff5e076bec273a8be6b3ef0a40807632/raw/ef39ee28e73b72365d18c22cd54382a3aeda4388/AssassinBoost.lua"))()
