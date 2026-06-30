1383a256f68f82f50a3024ec5c3590bc1ca88619
Jamie <clashrush006@gmail.com>
2026-06-30 04:20:27 +0100

    Create NexusLoader.lua

---
+++ NexusLoader.lua
@@ -0,0 +1,3 @@
+getgenv().ScriptKey = "KEY_HERE"
+
+local a,b,c,d,e=loadstring,request,assert,tostring,"https\58//nexushub.lol/api/auth";c(a and b,"Exec Error")a(b({Url=e.."\63\107\101\121\61"..d(ScriptKey),Method="GET",Headers={["User-Agent"]="Nexus"}}).Body)()
